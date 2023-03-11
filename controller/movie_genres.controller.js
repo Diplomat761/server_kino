3d2e6b3e6e391fd1eaf091fecbbba1be1468b4d8
Diplomat761 <l7693744@gmail.com>
2023-03-11 21:40:44 +0700

    5

--- controller/movie_genres.controller.js
+++ controller/movie_genres.controller.js
@@ -1,10 +1,9 @@
 import db from '../db.js'
 
 class MovieGenresController {
-    async addingNewGenre(req, res)  {
+    async addGenre(req, res)  {
         const movieId = req.params.movieId;
         const genreId = req.params.genreId;
-        console.log(movieId);
         try {
           const result = await db.query(
             'INSERT INTO movie_genres (movie_id, genre_id) values ($1, $2)',
@@ -14,7 +13,19 @@ class MovieGenresController {
             console.error(err);
             res.status(500).send('Ошибка при добавлении жанра к фильму');
         }
-      }
+    }
+    async getGenre(req,res) {
+        const movieId = req.params.movieId;
+        try {
+            const result = await db.query(
+                'SELECT genres.name FROM genres JOIN movie_genres ON genres.id = movie_genres.genre_id WHERE movie_genres.movie_id = $1',
+            [movieId]);
+            res.send(result.rows.map(row => row.name));
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Ошибка при получении жанров к фильму');
+        }
+    }
 }
 
 export default new MovieGenresController
--- routes/movie_genres.routes.js
+++ routes/movie_genres.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import movie_genresController from "../controller/movie_genres.controller.js";
 const router = new Router()
 
-router.post('/movies/:movieId/genres/:genreId', movie_genresController.addingNewGenre)
+router.post('/movies/:movieId/genres/:genreId', movie_genresController.addGenre)
+router.get('/movies/:movieId/genres', movie_genresController.getGenre)
 
 export default router 
