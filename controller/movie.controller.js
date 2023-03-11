52ed3bbfb79ba8994c19b4aa690d9843d3cb81e0
Diplomat761 <l7693744@gmail.com>
2023-03-11 22:23:44 +0700

    6

--- controller/movie.controller.js
+++ controller/movie.controller.js
@@ -7,5 +7,25 @@ class MovieController {
         [id, title, release_year])
         res.json(newMovie.rows[0])  
     }
+    async getMovies(req, res) {
+        const movies = await db.query('SELECT * FROM movies')
+        res.json(movies.rows)
+    }
+    async getOneMovie(req, res) {
+        const id = req.params.id
+        const movie = await db.query('SELECT * FROM movies where id = $1', [id])
+        res.json(movie.rows[0])
+    }
+    async updateMovie(req,res) {
+        const {id, title, release_year} = req.body
+        const movie = await db.query(`UPDATE movies set release_year = $3, title = $2 where id = $1 RETURNING *`,
+        [id, title, release_year])
+        res.json(movie.rows[0]) 
+    }
+    async deleteMovie(req, res) {
+        const id = req.params.id
+        const movie = await db.query('DELETE FROM movies where id = $1', [id])
+        res.json(movie.rows[0])
+    }
 }
 export default new MovieController()
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -3,5 +3,9 @@ import movieController from "../controller/movie.controller.js";
 const router = new Router()
 
 router.post('/movie', movieController.createMovie)
+router.get('/movie', movieController.getMovies)
+router.get('/movie/:id', movieController.getOneMovie)
+router.put('/movie', movieController.updateMovie)
+router.delete('/movie/:id', movieController.deleteMovie)
 
 export default router
