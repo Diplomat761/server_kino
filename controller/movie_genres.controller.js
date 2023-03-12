669e71d3bfbff1d31b3106ebcf9f989f8bd0d141
Diplomat761 <l7693744@gmail.com>
2023-03-12 20:19:23 +0700

    7

--- controller/movie_genres.controller.js
+++ controller/movie_genres.controller.js
@@ -1,7 +1,7 @@
 import db from '../db.js'
 
 class MovieGenresController {
-    async addGenre(req, res)  {
+    async addMovieGenres(req, res)  {
         const movieId = req.params.movieId;
         const genreId = req.params.genreId;
         try {
@@ -14,16 +14,31 @@ class MovieGenresController {
             res.status(500).send('Ошибка при добавлении жанра к фильму');
         }
     }
-    async getGenre(req,res) {
+    async getMovieGenres(req,res) {
         const movieId = req.params.movieId;
         try {
             const result = await db.query(
-                'SELECT genres.name FROM genres JOIN movie_genres ON genres.id = movie_genres.genre_id WHERE movie_genres.movie_id = $1',
+            'SELECT genres.name FROM genres JOIN movie_genres ON genres.id = movie_genres.genre_id WHERE movie_genres.movie_id = $1',
             [movieId]);
             res.send(result.rows.map(row => row.name));
         } catch (err) {
             console.error(err);
             res.status(500).send('Ошибка при получении жанров к фильму');
+        }
+    }
+    async deleteMovieGenres(req, res) {
+        const movieId = req.params.movieId;
+        const genreId = req.params.genreId;
+        
+        try {
+          const result = await db.query(
+            'DELETE FROM movie_genres WHERE movie_id = $1 AND genre_id = $2',
+            [movieId, genreId]
+          );
+          res.send(`Жанр ${genreId} удален из фильма ${movieId}`);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Ошибка при удалении жанра из фильма');
         }
     }
 }
--- routes/movie_genres.routes.js
+++ routes/movie_genres.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import movie_genresController from "../controller/movie_genres.controller.js";
 const router = new Router()
 
-router.post('/movies/:movieId/genres/:genreId', movie_genresController.addGenre)
-router.get('/movies/:movieId/genres', movie_genresController.getGenre)
+router.post('/movies/:movieId/genres/:genreId', movie_genresController.addMovieGenres)
+router.get('/movies/:movieId/genres', movie_genresController.getMovieGenres)
+router.delete('/movies/:movieId/genres/:genreId', movie_genresController.deleteMovieGenres)
 
 export default router 
